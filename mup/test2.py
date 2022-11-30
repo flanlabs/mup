d621412de8bece4e7c55f5e1160fe228f3b2ccde
Zygimantas Straznickas <hi@zygi.me>
2022-11-30 00:19:12 +0000

    WIP2

--- mup/test2.py
+++ mup/test2.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 import torch.nn as nn
 import torch
 
@@ -11,8 +11,11 @@ from composer.utils import dist
 import composer.utils
 
 import mup
+import mup.shape
 import mup.init
 from mup.layer import get_infshape_of_param_name
+
+import composer.optim
 
 import torch.distributed.fsdp.flatten_params_wrapper
 
@@ -52,19 +55,116 @@ def expand_params(model: nn.Module):
 
     return params_with_shapes
 
-# class HackedAdamW(torch.optim.AdamW):
+class HackedAdamW(composer.optim.DecoupledAdamW):
+    # infshape_map_by_id: Dict[str, ]
+    params_to_module: Dict[torch.Tensor, nn.Module] = {}
+    param_names: Dict[torch.Tensor, List[str]] = {}
 
-#     infshape_map_by_id: 
+    def __init__(self, model: nn.Module, infshapes: Dict[str, mup.InfShape], *args, **kwargs):
+        self.model = model
+        self.infshapes = infshapes
 
-#     def __init__(self, model: nn.Module, *args, **kwargs):
-#         self.model = model
+        super().__init__(*args, **kwargs)
+        # print(self.infshapes)
+
+    def add_param_group(self, param_group: dict) -> None:
+        self.params_to_module = {}
+        self.param_names = {}
+
+        def form_name(m_name: str, p_name: str):
+            res = m_name + "." + p_name
+            return res.replace("_fsdp_wrapped_module.", "").replace("_fpw_module.", "")
+
+        for m_name, m in self.model.named_modules():
+            for p_name, p in m.named_parameters():
+                
+                self.params_to_module[p] = m
+                if isinstance(p, torch.distributed.fsdp.flatten_params_wrapper.FlatParameter):
+                    self.param_names[p] = [form_name(m_name, n.param_name) for n in p._param_infos]
+                else:
+                    self.param_names[p] = [form_name(m_name, p_name)]
+
+        # print(param_names)
+
+        p: torch.Tensor
+        for p in param_group["params"]:
+            assert p in self.params_to_module, "Trying to add a parameter that's not preregistered"
+        return super().add_param_group(param_group)
 
 
-#     def add_param_group(self, param_group: dict) -> None:
-#         p: torch.Tensor
-#         for p in param_group["params"]:
-#             if p.
-#         return super().add_param_group(param_group)
+    @torch.no_grad()
+    def step(self, closure=None):
+        """Performs a single optimization step.
+        Args:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            params_with_grad = []
+            grads = []
+            exp_avgs = []
+            exp_avg_sqs = []
+            max_exp_avg_sqs = []
+            state_steps = []
+            amsgrad = group['amsgrad']
+            beta1, beta2 = group['betas']
+            eps = group['eps']
+            lr = group['lr']
+            initial_lr = group['initial_lr']
+            weight_decay = group['weight_decay']
+
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                params_with_grad.append(p)
+                if p.grad.is_sparse:
+                    raise RuntimeError('AdamW does not support sparse gradients')
+                grads.append(p.grad)
+
+                state = self.state[p]
+
+                # State initialization
+                if len(state) == 0:
+                    state['step'] = 0
+                    # Exponential moving average of gradient values
+                    state['exp_avg'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                    # Exponential moving average of squared gradient values
+                    state['exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                    if amsgrad:
+                        # Maintains max of all exp. moving avg. of sq. grad. values
+                        state['max_exp_avg_sq'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+
+                exp_avgs.append(state['exp_avg'])
+                exp_avg_sqs.append(state['exp_avg_sq'])
+
+                if amsgrad:
+                    max_exp_avg_sqs.append(state['max_exp_avg_sq'])
+
+                # update the steps for each param group update
+                state['step'] += 1
+                # record the step after step update
+                state_steps.append(state['step'])
+
+            self.adamw(params_with_grad,
+                       grads,
+                       exp_avgs,
+                       exp_avg_sqs,
+                       max_exp_avg_sqs,
+                       state_steps,
+                       amsgrad=amsgrad,
+                       beta1=beta1,
+                       beta2=beta2,
+                       lr=lr,
+                       initial_lr=initial_lr,
+                       weight_decay=weight_decay,
+                       eps=eps)
+
+        return loss
 
 if __name__ == "__main__":
     print("wawawa")
@@ -77,6 +177,8 @@ if __name__ == "__main__":
     a: A = mup.set_base_shapes(a, a)
 
     a.initia()
+
+    infshapes = mup.shape.get_infshapes(a)
 
     # print([b for (_, b) in expand_params(a)])
 
@@ -105,16 +207,19 @@ if __name__ == "__main__":
 
     b = FSDP(a, auto_wrap_policy=sharding_strategy)
 
-    opt = torch.optim.SGD([x for (x, _) in expand_params(b)], lr=1e-3)
+    opt = HackedAdamW(b, infshapes, b.parameters(), lr=1e-3)
 
+    opt.add_param_group({"params": b.parameters()})
+
+    mup.optim
 
     torch.optim.AdamW
 
-    opt.zero_grad()
-    output = b(rand_inp)
-    loss = torch.nn.MSELoss()(output, rand_out)
-    loss.backward()
-    opt.step()
+    # opt.zero_grad()
+    # output = b(rand_inp)
+    # loss = torch.nn.MSELoss()(output, rand_out)
+    # loss.backward()
+    # opt.step()
 
     # print([z for (_, z) in expand_params(b)])
     # print(b)
